33463a18a3860f7d9d6a18633dc20337f7d9c5bf
Spoonroke <radiantfuture@mail.ru>
2017-10-31 21:36:49 +0400

    Changes for Artem

--- game.js
+++ game.js
@@ -1,43 +1,55 @@
-window.onload = init;
+window.onload = init;    // функц., запускающаяся в начале
+
+/*
+В массив добавляются x,y по правило чётные числа - x; нечётные - y -> var i=1,100,i+=2 - нечётные (y)
+*/
 
 var canvas;
 var screen;
-var x = 380;
+var x = 380;    //x,y игрока
 var y = 300;
-var up,down,left,right;   //flags for control
+var health = 10;      //hp
+var up,down,left,right, shoot;   //flags for control
+var gameFlag = true;              // новый костыль (для выхода с уровня)
 
-//todo UNIT BUFFER
+var player;
+var unit;
+
+//массив врагов пустой, потому что будут добавляться в функции рандом
 var units = [];
 
 function init(){
-  canvas = document.getElementById("canvas");
+  canvas = document.getElementById("canvas"); //конвенция
   screen = canvas.getContext("2d");
-  randCoord();
-  tick();
+  player = new Player();
+  unit = new Unit();
+  units[0] = unit();
+  units[0].x = 25; units[0].y = 25;
+
+  if(units[0] == null){console.log("you are eblan");}
+  randCoord();  //задаём рандомные координаты противникам и заносим в массив
+  tick();       //игровой цикл
 }
 
-function drawRect(x,y,w,h) {
-  screen.fillStyle = "#E4E4E4"
-  screen.fillRect(x,y,w,h);
-}
 
-
-//TODO проверка на выход за границы поля
+//чекает флаги и изменяет x,y
 function moveRect(e){
+  //условие с y уберёшь, дебил
   if(up == true & y>0){
-    y -= 10;
+    player.y -= 10;
   }
   if(down == true & y<550){
-    y += 10;
+    player.y += 10;
   }
   if(left == true & x>0){
-    x -= 15;
+    player.x -= 10;
   }
   if(right == true & x<750){
-    x += 15;
+    player.x += 10;
   }
 }
 
+//слушает нажатия клавиш
 function keyListener(){
   window.onkeydown = function(e){
     switch(e.keyCode){
@@ -52,6 +64,9 @@ function keyListener(){
       break;
       case 40:
         down = true;
+      break;
+      case 32:
+        shoot = true;
       break;
     }
   }
@@ -70,45 +85,86 @@ function keyListener(){
       case 40:
         down = false;
       break;
+      case 32:
+        shoot = false;
+      break;
     }
   }
 }
 
+//игровой цикл
 function tick(){
-  screen.clearRect(0, 0, canvas.width, canvas.height);
+  screen.clearRect(0, 0, canvas.width, canvas.height);  //очищение экрана
 
-  drawRect(x,y,50,50);
   moveRect("keydown");
   keyListener();
-  step1();
-  requestAnimationFrame(tick);
+
+  if (gameFlag == true)
+  {
+  units[0].draw();
+  player.draw();
+
+  }
+  else{gameOver();}
+  requestAnimationFrame(tick);  //ограничивает fps
 }
 
-function step1(){
-//
+//уровень первый и единственный
+/*function step1(){
   for (var i = 1; i < 100; i+=2){
-
+                                                                            //ечли Ex = 200, x = 210
     if((units[i - 1] <= (x + 50) & units[i - 1] >= x-10)
      &                                                           //проверка на столкновение
      (units[i] <= (y + 50) & units[i] >= y-10))
      {
-
-       units[i - 1] = Math.floor(Math.random() * 800);
-       units[i] = Math.floor(Math.random() * -3000);
+       delete units[i]; delete units[i - 1];
+       health -= 1;
        console.log("Столкновение");
      }
 
-    if(units[i] > 600) {units[i] = Math.floor(Math.random() * -3000)}    //проверка на исчезновение
+    if(units[i] > 600) {delete units[i]; delete units[i - 1]; health -= 1;}    //проверка на исчезновение
+
+    if (shoot == true){                                                           //пока пробел зажат, мы отрисовываем и проверяем на то, столкнулся ли луч.
+      screen.fillStyle = "#E4E4E4"; screen.fillRect(x+20, 0, 10, y);
+      if(units[i-1] > x & units[i-1] < x+50 & units[i] > 0) {delete units[i]; delete units[i-1];}
+    }
 
     units[i] += 10;    //движение точек
-    drawRect(units[i - 1], units[i], 10,10);    //отрисовка
+    drawRect(units[i - 1], units[i], 20,20);    //отрисовка
+
+    if (health == 0) {gameFlag = false};
   }
-}
+}*/
 
 function randCoord(){
   // add units with random coordinates to massive
   for (var i = 0; i < 100; i++){
-    if (i % 2 == 0){units[i] = Math.floor(Math.random() * 800);}
-    else {units[i] = Math.floor(Math.random() * -3000)}
+    if (i % 2 == 0){units[i] = Math.floor(Math.random() * 800);}  // округление рандома от 0,1*800
+    else {units[i] = Math.floor(Math.random() * -20000)}
   }
+}
+
+function gameOver(){
+  screen.font = "50px Verdana";  //стиль
+  screen.fillText("Game Over", 260, 400);
+}
+
+function Unit(){
+  this.x;
+  this.y;
+}
+
+Unit.prototype.draw = function(){
+  screen.fillStyle = "#E4E5E4";
+  screen.fillRect(this.x,this.y,25,25);
+}
+
+function Player(){
+  this.x = 380;
+  this.y = 300;
+}
+
+Player.prototype.draw = function(){
+  screen.fillStyle = "#E4E4E4";
+  screen.fillRect(this.x,this.y,50,50);
 }
